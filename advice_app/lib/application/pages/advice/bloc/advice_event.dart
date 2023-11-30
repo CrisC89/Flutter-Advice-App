496a67005d5a32e9d581af8be9f21ed8da8634bb
Cristina Cavaleri <cavaleri.cristina@outlook.be>
2023-11-30 11:24:42 +0100

    Test equatable extension

--- advice_app/lib/application/pages/advice/bloc/advice_event.dart
+++ advice_app/lib/application/pages/advice/bloc/advice_event.dart
@@ -1,7 +1,10 @@
 part of 'advice_bloc.dart';
 
 @immutable
-abstract class AdviceEvent {}
+abstract class AdviceEvent extends Equatable{
+  @override
+  List<Object?> get props => [];
+}
 
 class AdviceRequestedEvent extends AdviceEvent{
 
